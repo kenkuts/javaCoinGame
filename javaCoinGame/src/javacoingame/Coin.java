89ca3cb6d71228ef4c7b94fadb64162dbbb31e78
Kenneth Kutschera <kennethchoi19@gmail.com>
2016-11-13 04:26:09 -0500

    Coin Game

---
+++ javaCoinGame/src/javacoingame/Coin.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javacoingame;
+import java.util.Random;
+ 
+/**
+ *
+ * @author Kenneth R. Kutschera
+ */
+public class Coin {
+    
+    private CoinFace sideUp;
+    
+    public Coin(){
+        Random rand = new Random();
+        int coin = rand.nextInt(1);
+        
+        if (coin == 1){
+            this.sideUp = CoinFace.HEADS;
+        }
+        else {
+            this.sideUp = CoinFace.TAILS;
+        }
+    }
+    
+    public void tossCoin(){
+        Random rand = new Random();
+        int coin = rand.nextInt(1);
+        
+        if (coin == 1){
+            this.sideUp = CoinFace.HEADS;
+        }
+        else {
+            this.sideUp = CoinFace.TAILS;
+        }
+    }
+    
+    public CoinFace getSideUp(){
+       
+        return sideUp;
+    }
+     
+    public String toSTring(){
+        
+        return " " + sideUp;
+    }
+}
---
+++ javaCoinGame/src/javacoingame/JavaCoinGame.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javacoingame;
+
+/**
+ *
+ * @author Kenneth R. Kutschera
+ */
+public class JavaCoinGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int heads = 0;
+        int tails = 0;
+        int r; 
+        Coin toss = new Coin();
+        
+        System.out.println("The coin is initially facing" + toss);
+        
+        System.out.println("Let's start the game!!!");
+        
+        for (r = 0; r =< 20; r++)
+        {
+            System.out.println("This is coin toss # " + r);
+            toss.tossCoin();
+            System.out.println("The coins face is now " + toss.getSideUp());
+            if (toss.getSideUp().equals(CoinFace.HEADS))
+            {
+                heads++;
+            }
+            else {
+                tails++;
+            }
+            
+        }
+        
+        System.out.println("The coin landed on heads " + heads + " times.");
+        System.out.println("The coin landed on tails " + tails + " times.");
+    }
+    
+}
